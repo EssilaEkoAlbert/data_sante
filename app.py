72c5fc5f0c9e4e897c1e501e606b91f310075b76
EssilaEkoAlbert <ekoalbert521@gmail.com>
2026-04-30 21:24:46 +0200

    Refonte complete app.py avec affichage carte

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, Response
-import csv
+import folium
 import io
+import csv
 
 app = Flask(__name__)
 
 
 donnees_sante = []
+
+
+coords = {
+    "Littoral": [4.05, 9.70],
+    "Centre": [3.84, 11.50],
+    "Ouest": [5.47, 10.41]
+}
 
 @app.route('/')
 def index():
@@ -15,32 +23,51 @@ def index():
 def enregistrer():
     zone = request.form.get('zone')
     age = request.form.get('age')
-    diagnostic = request.form.get('diagnostic')
-    
-    
-    if zone and age and diagnostic:
+    maladie = request.form.get('maladie')
+
+    if zone and age and maladie:
         donnees_sante.append({
             'zone': zone,
             'age': age,
-            'diagnostic': diagnostic
+            'diagnostic': maladie
         })
     return redirect(url_for('dashboard'))
 
 @app.route('/dashboard')
 def dashboard():
-    # POINT 1 & 5 : Visualisation et Carte
-    return render_template('dashboard.html', donnees=donnees_sante)
+    
+    ma_carte = folium.Map(location=[7.3697, 12.3547], zoom_start=6)
+
+    
+    for d in donnees_sante:
+        zone_nom = d['zone']
+        if zone_nom in coords:
+            folium.CircleMarker(
+                location=coords[zone_nom],
+                radius=10,
+                color="red",
+                fill=True,
+                fill_color="red",
+                popup=f"{d['diagnostic']} (Âge: {d['age']})"
+            ).add_to(ma_carte)
+
+    carte_html = ma_carte._repr_html_()
+    return render_template('dashboard.html', carte=carte_html, donnees=donnees_sante)
 
 @app.route('/export')
 def export():
-    # POINT 4 : Exportation CSV (ouvrable dans Excel)
     si = io.StringIO()
     cw = csv.writer(si)
     cw.writerow(['Zone de Sante', 'Age', 'Diagnostic'])
     for d in donnees_sante:
         cw.writerow([d['zone'], d['age'], d['diagnostic']])
+    
     output = si.getvalue()
-    return Response(output, mimetype="text/csv", headers={"Content-disposition":"attachment; filename=rapport_sante.csv"})
+    return Response(
+        output,
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=donnees_sanitaires.csv"}
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
